b2cbcd5979fe3cebd57235c36bf20129dad938da
Андрей Никитин <146591318+AndrewNikkk@users.noreply.github.com>
2024-11-20 23:18:49 +0300

    sameLetterPattern && memeSum

--- Tasks/FifthTask.java
+++ Tasks/FifthTask.java
@@ -12,6 +12,11 @@ public class FifthTask {
         System.out.println(sameLetterPattern("FFGG", "CDCD"));
         System.out.println(sameLetterPattern("FFFF", "ABCD"));
 
+        System.out.println("--------------------------[ 2 ]--------------------------");
+
+        System.out.println(memeSum(26, 39));
+        System.out.println(memeSum(122, 81));
+        System.out.println(memeSum(1222, 30277));
     
     }
 
@@ -35,6 +40,34 @@ public class FifthTask {
             arr2[i] = map2.computeIfAbsent(chars2[i], k -> count2.incrementAndGet());
         }
         return Arrays.equals(arr1, arr2);
+    }
+
+    public static int memeSum(int num1, int num2) {
+        StringBuilder str1 = new StringBuilder(String.valueOf(Math.max(num1, num2)));
+        StringBuilder str2 = new StringBuilder(String.valueOf(Math.min(num1, num2)));
+        str2.insert(0, "0".repeat(str1.length() - str2.length()));
+
+        System.out.println(str1);
+        System.out.println(str2);
+
+        
+
+        StringBuilder res = new StringBuilder();
+        int result = 0;
+
+        for (int i = Math.max(str1.length(), str2.length()); i > 0; i--) {
+            int index1 = i - 1;
+            int digit1 = (index1 < str1.length() ? Character.getNumericValue(str1.charAt(index1)) : 0);
+            
+            int index2 = i - 1;
+            int digit2 = (index2 < str2.length() ? Character.getNumericValue(str2.charAt(index2)) : 0);
+        
+            res.insert(0, String.valueOf(digit1 + digit2)); // Добавляем цифры в начало строки
+        }
+
+        result = Integer.parseInt(res.toString());
+        return result;
+        
     }
 }
 
